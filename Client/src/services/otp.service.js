0a83c8bd9a65cc8a5ced9dbcd91165853fd0c75c
EXCELLENCE ALLEGIANCE PRIVATE LIMITED <excellenceallegiance25@gmail.com>
2026-02-13 11:23:19 +0530

    Reset git index and fixed folder structure

--- Client/src/services/otp.service.js
+++
@@ -1,4 +0,0 @@
-// services/otp.service.js
-export const generateOTP = () => {
-  return Math.floor(100000 + Math.random() * 900000).toString();
-};
--- Server/utils/database.js
+++
@@ -1,6 +0,0 @@
-// utils/database.js
-export const database = {
-  employees: [], // registered employees
-  otps: [], // OTP records
-  users: [], // future use (admin / manager)
-};
